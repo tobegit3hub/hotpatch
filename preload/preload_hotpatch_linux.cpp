d7246814d950149a8700568c5f1a9f93c0f011ea
tobe <tobeg3oogle@gmail.com>
2020-05-19 11:30:37 +0800

    Implement the preload library for linux

--- preload/preload_hotpatch_linux.cpp
+++ preload/preload_hotpatch_linux.cpp
@@ -1,6 +1,10 @@
 #include<stdio.h>
+#include "hotpatch_server.h"
 
 // LD_PRELOAD="./libpreload_hotpatch_linux.so" /bin/echo a b c d
 static void __attribute__((constructor)) ctor() {
     printf("---------- Welcome to hotpatch ----------\n");
+
+    // This requires to build the shared library with "-fPIC"
+    hotpatch::InitHotpatchServer();
 }
