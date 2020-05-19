d8abfa6b3ada01eb424854205ff933d87b6ebf4e
tobe <tobeg3oogle@gmail.com>
2020-05-19 16:42:33 +0800

    Upgrade python client to 0.1.0

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hotpatch",
-    version="0.0.1",
+    version="0.1.0",
     author="tobe",
     author_email="tobeg3oogle@gmail.com",
     url="https://github.com/tobegit3hub/hotpatch",
