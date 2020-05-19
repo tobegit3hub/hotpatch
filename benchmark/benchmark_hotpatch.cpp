1445d48980dc2df5fc603a9819dfbd75a5d7fb88
tobe <tobeg3oogle@gmail.com>
2020-05-19 17:33:21 +0800

    Update the benchmark with fibonacci function

--- benchmark/benchmark_hotpatch.cpp
+++ benchmark/benchmark_hotpatch.cpp
@@ -15,12 +15,16 @@ static timestamp_t get_timestamp () {
     return  now.tv_usec + (timestamp_t)now.tv_sec * 1000000;
 }
 
-int add_one(int a) {
-    return a+1;
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
 }
 
 int main(int argc, char **argv) {
-    const int BENCHMARK_ITERATION = 1000000000;
+    const int BENCHMARK_ITERATION = 1;
     timestamp_t start;
     timestamp_t end;
     double duration;
@@ -29,25 +33,25 @@ int main(int argc, char **argv) {
     hotpatch_server->Init();
 
     // Test native C++ function
+    printf("start");
     start = get_timestamp();
     for (int i=0; i<BENCHMARK_ITERATION; ++i) {
-        add_one(1);
+        fibonacci(45);
     }
+    printf("end");
     end = get_timestamp();
     duration = (end - start) / 1000000.0L;
     cout << "Native call time: " << duration << endl;
 
     // Register and upgrade to new function implementation
-    hotpatch_server->RegisterFunction("add_one", (void*)add_one);
-    hotpatch_server->GetHotpathCommand()->HandleLibLoad("add_one_lib", "./examples/libadd_one_patch1.so");
-    hotpatch_server->GetHotpathCommand()->HandleFuncUpgrade("add_one_lib", "add_one");
-
-    //std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    hotpatch_server->RegisterFunction("fibonacci", (void*)fibonacci);
+    hotpatch_server->GetHotpathCommand()->HandleLibLoad("fibonacci_patch1", "./examples/libfibonacci_patch1.so");
+    hotpatch_server->GetHotpathCommand()->HandleFuncUpgrade("fibonacci_patch1", "fibonacci");
 
     // Test hotpatch C++ function
     start = get_timestamp();
     for (int i=0; i<BENCHMARK_ITERATION; ++i) {
-        add_one(1);
+        fibonacci(45);
     }
     end = get_timestamp();
     duration = (end - start) / 1000000.0L;
---
+++ examples/fibonacci_func.cpp
@@ -0,0 +1,7 @@
+extern "C" int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
