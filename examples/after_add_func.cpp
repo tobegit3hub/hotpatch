ada64fcfa048da4ddf1dd1bb68ef10fefcb7569d
tobe <tobeg3oogle@gmail.com>
2020-05-19 16:58:20 +0800

    Remove the before and after function in examples

--- examples/after_add_func.cpp
+++
@@ -1,6 +0,0 @@
-
-#include <iostream>
-
-extern "C" void after_add_func() {
-    std::cout << "Call after add func" << std::endl;
-}
--- examples/before_add_func.cpp
+++
@@ -1,6 +0,0 @@
-
-#include <iostream>
-
-extern "C" void before_add_func() {
-    std::cout << "Call before add func" << std::endl;
-}
